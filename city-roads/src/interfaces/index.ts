d06745316d4cd54610410677ce6bf08c959de359
DrEmbryo <mcredpower@gmail.com>
2023-07-01 14:48:21 +0200

    update types

--- city-roads/src/interfaces/index.ts
+++ city-roads/src/interfaces/index.ts
@@ -1,4 +1,14 @@
-export type Node = [string, string];
-export type GraphEdge = Array<Node>;
-export type GraphEdges = Array<GraphEdge>;
 export type Graph = { [key: string]: string[] };
+export type GraphNode = [string, string];
+export type GraphEdge = Array<GraphNode>;
+export type GraphEdges = Array<GraphEdge>;
+
+export type BreathFirstSearch = (
+  graph: Graph,
+  startPosition: string,
+  endPosition: string,
+  visitedNodes: Set<string>
+) => {};
+
+export type NodeQueueItem = { node: string; path: string };
+export type NodeQueue = Array<NodeQueueItem>;
