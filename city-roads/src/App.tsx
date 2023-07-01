1f61f9eaea359ee7ad69afe3c5b830990f14416b
DrEmbryo <mcredpower@gmail.com>
2023-07-01 15:56:54 +0200

    update ui to match task ac

--- city-roads/src/App.tsx
+++ city-roads/src/App.tsx
@@ -1,3 +1,49 @@
+import { useState } from "react";
+
+import {
+  Card,
+  CardBody,
+  CardFooter,
+  Button,
+  Typography,
+} from "@material-tailwind/react";
+import { CitySelect } from "./components/select";
+
+import { edges } from "./constants";
+import { buildGraph } from "./utils/buildGraph";
+import { breathFirstSearch } from "./utils/breathFirstSearch";
+
 export const App = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  const [inputs, setInputs] = useState({ from: "", to: "" });
+  const [searchResult, setSearchResult] = useState("");
+  const graph = buildGraph(edges);
+  const cities = Object.keys(graph);
+
+  const handleInputs = (field: "from" | "to", value: string) => {
+    const current = { ...inputs };
+    current[field] = value;
+    setInputs(current);
+  };
+
+  const handleFindPath = () => {
+    const result = breathFirstSearch(graph, inputs.from, inputs.to, new Set());
+    setSearchResult(result);
+  };
+
+  return (
+    <div className="flex h-screen bg-gray-700">
+      <Card className="w-96 m-auto">
+        <CardBody className="flex flex-col gap-4">
+          <CitySelect label="from" cities={cities} onChange={handleInputs} />
+          <CitySelect label="to" cities={cities} onChange={handleInputs} />
+          {searchResult && <Typography>{searchResult}</Typography>}
+        </CardBody>
+        <CardFooter className="pt-0">
+          <Button variant="gradient" fullWidth onClick={handleFindPath}>
+            Find Path
+          </Button>
+        </CardFooter>
+      </Card>
+    </div>
+  );
 };
--- city-roads/tailwind.config.js
+++ city-roads/tailwind.config.js
@@ -1,8 +1,10 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+import withMT from "@material-tailwind/react/utils/withMT";
+
+export default withMT({
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+});
